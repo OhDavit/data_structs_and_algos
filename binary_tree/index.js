8a997588d81ddf856989e6623c153d4b76b8b0b2
Davit Ohanyan <davitohanyan@gmail.com>
2017-05-08 17:24:27 +0200

    improves code style

--- binary_tree/index.js
+++ binary_tree/index.js
@@ -1,42 +1,43 @@
 "use strict";
 
 class Node {
-	constructor(data) {
-	   this.left = null; // points to smaller subtrees
-	   this.right = null; // points to bigger subtree
-	   this.data = data;	
-	}
+  constructor(data) {
+    this.left = null; // points to smaller subtrees
+    this.right = null; // points to bigger subtree
+    this.data = data;
+  }
 }
 
 class BinaryTree {
   constructor() {
-  	this.root = null;
+    this.root = null;
   }
 
   print() {
-  	this._printRecursively(this.root);
+    this._printRecursively(this.root);
   }
 
   _printRecursively(node) {
-  	if(!node) {
-  		return;
-  	}
+    if (!node) {
+      return;
+    }
 
-  	this._printRecursively(node.left);
+    this._printRecursively(node.left);
     console.log(node.data + ' ');
     this._printRecursively(node.right);
   }
- /*
-  Returns the max root-to-leaf depth of the tree. 
- */
+
+  /*
+   Returns the max root-to-leaf depth of the tree. 
+   */
   maxDepth() {
-  	return this._maxDepthRecursively(this.root);
+    return this._maxDepthRecursively(this.root);
   }
 
   _maxDepthRecursively(node) {
-  	if(!node) {
-  		return 0;
-  	}
+    if (!node) {
+      return 0;
+    }
     return 1 + Math.max(this._maxDepthRecursively(node.left), this._maxDepthRecursively(node.right));
   }
 
@@ -45,36 +46,36 @@ class BinaryTree {
   }
 
   _lookupRecursively(node, data) {
-     if(node.data === data) {
-     	return true;
-     }
+    if (node.data === data) {
+      return true;
+    }
 
-     if(node.data > data) {
-     	return this._lookupRecursively(this.left, data);
-     } 
+    if (node.data > data) {
+      return this._lookupRecursively(this.left, data);
+    }
 
-     return this._lookupRecursively(this.right, data);
+    return this._lookupRecursively(this.right, data);
   }
 
   insert(data) {
-  	this.root = this._insertRecursively(this.root, data);
+    this.root = this._insertRecursively(this.root, data);
   }
 
- /** 
-   Recursive insert -- given a node pointer, recur down and 
-   insert the given data into the tree. Returns the new 
-   node pointer (the standard way to communicate 
-   a changed pointer back to the caller). 
-  */ 
+  /**
+   Recursive insert -- given a node pointer, recur down and
+   insert the given data into the tree. Returns the new
+   node pointer (the standard way to communicate
+   a changed pointer back to the caller).
+   */
   _insertRecursively(node, data) {
-  	if(!node) {
-  		return new Node(data);
-  	}
+    if (!node) {
+      return new Node(data);
+    }
 
-    if(data <= node.data) {
-     	node.left = this._insertRecursively(node.left, data);
+    if (data <= node.data) {
+      node.left = this._insertRecursively(node.left, data);
     } else {
-    	node.right = this._insertRecursively(node.right, data);
+      node.right = this._insertRecursively(node.right, data);
     }
 
     return node;
@@ -88,6 +89,3 @@ bTree.insert(25);
 bTree.insert(32);
 bTree.print();
 console.log('depth = ' + bTree.maxDepth());
-
-
-
